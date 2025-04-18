37a2fa25e6bca7f59a91809e0a9691bb9407ff19
Babur Makhmudov <bmuddha13@gmail.com>
2025-04-18 15:06:31 +0400

    chore(assist): cleanup report command code

--- assist/src/report.rs
+++ assist/src/report.rs
@@ -1,86 +1,14 @@
-use core::{consts::RUNS_OUTPUT_PATH, stats::BenchStatistics, types::BenchResult};
 use std::{fs, path::PathBuf};
 
+use core::{consts::RUNS_OUTPUT_PATH, stats::BenchStatistics, types::BenchResult};
+
 use json::JsonContainerTrait;
+use prettytable::{Cell, Row, Table};
 
 pub fn report(path: Option<PathBuf>) -> BenchResult<()> {
     let path = path.unwrap_or_else(latest_output_path);
     let output = fs::read_to_string(path)?;
     let stats: BenchStatistics = json::from_str(&output)?;
-    use prettytable::{Cell, Row, Table};
-
-    macro_rules! add_stats_row {
-        ($table:expr, $label:expr, $stats:expr) => {
-            $table.add_row(Row::new(vec![
-                Cell::new($label),
-                Cell::new(&$stats.count.to_string()),
-                Cell::new(&$stats.median.to_string()),
-                Cell::new(&$stats.min.to_string()),
-                Cell::new(&$stats.max.to_string()),
-                Cell::new(&$stats.avg.to_string()),
-                Cell::new(&$stats.quantile95.to_string()),
-                Cell::new(&$stats.stddev.to_string()),
-            ]));
-        };
-    }
-
-    fn print_stats_pretty(stats: &BenchStatistics) {
-        let mut table = Table::new();
-
-        table.add_row(Row::new(vec![
-            Cell::new("Configuration Key"),
-            Cell::new("Value"),
-        ]));
-
-        if let Some(obj) = stats.configuration.as_object() {
-            for (key, value) in obj {
-                let mut inner_content = String::new();
-                if let Some(inner_obj) = value.as_object() {
-                    for (inner_key, inner_value) in inner_obj {
-                        inner_content.push_str(&format!("{}: {}\n", inner_key, inner_value));
-                    }
-                }
-                table.add_row(Row::new(vec![
-                    Cell::new(key),
-                    Cell::new(inner_content.trim_end()),
-                ]));
-            }
-        }
-
-        table.printstd();
-
-        let mut table = Table::new();
-
-        table.add_row(Row::new(vec![
-            Cell::new("Metric"),
-            Cell::new("Observations"),
-            Cell::new("Median"),
-            Cell::new("Min"),
-            Cell::new("Max"),
-            Cell::new("Avg"),
-            Cell::new("95th Percentile"),
-            Cell::new("Stddev"),
-        ]));
-
-        add_stats_row!(table, "HTTP Requests Latency", stats.http_requests_latency);
-        add_stats_row!(
-            table,
-            "Account Update Latency",
-            stats.account_update_latency
-        );
-        add_stats_row!(
-            table,
-            "Signature Confirmation Latency",
-            stats.signature_confirmation_latency
-        );
-        add_stats_row!(
-            table,
-            "Transactions Per Second",
-            stats.transactions_per_second
-        );
-
-        table.printstd();
-    }
 
     print_stats_pretty(&stats);
     Ok(())
@@ -95,4 +23,81 @@ fn latest_output_path() -> PathBuf {
     outputs
         .pop()
         .expect("benchmark runs output directory should have at least one entry")
+}
+
+macro_rules! add_stats_row {
+    ($table:expr, $label:expr, $stats:expr) => {
+        $table.add_row(Row::new(vec![
+            Cell::new($label),
+            Cell::new(&$stats.count.to_string()),
+            Cell::new(&$stats.median.to_string()),
+            Cell::new(&$stats.min.to_string()),
+            Cell::new(&$stats.max.to_string()),
+            Cell::new(&$stats.avg.to_string()),
+            Cell::new(&$stats.quantile95.to_string()),
+            Cell::new(&$stats.stddev.to_string()),
+        ]));
+    };
+}
+
+fn print_stats_pretty(stats: &BenchStatistics) {
+    let mut table = Table::new();
+
+    table.add_row(Row::new(vec![
+        Cell::new("Configuration Key"),
+        Cell::new("Value"),
+    ]));
+
+    if let Some(obj) = stats.configuration.as_object() {
+        for (key, value) in obj {
+            let mut inner_content = String::new();
+            if let Some(inner_obj) = value.as_object() {
+                for (inner_key, inner_value) in inner_obj {
+                    inner_content.push_str(&format!("{}: {}\n", inner_key, inner_value));
+                }
+            }
+            table.add_row(Row::new(vec![
+                Cell::new(key),
+                Cell::new(inner_content.trim_end()),
+            ]));
+        }
+    }
+
+    table.printstd();
+
+    let mut table = Table::new();
+
+    table.add_row(Row::new(vec![
+        Cell::new("Metric"),
+        Cell::new("Observations"),
+        Cell::new("Median"),
+        Cell::new("Min"),
+        Cell::new("Max"),
+        Cell::new("Avg"),
+        Cell::new("95th Percentile"),
+        Cell::new("Stddev"),
+    ]));
+
+    add_stats_row!(
+        table,
+        "HTTP Requests Latency (μs)",
+        stats.http_requests_latency
+    );
+    add_stats_row!(
+        table,
+        "Account Update Latency (μs)",
+        stats.account_update_latency
+    );
+    add_stats_row!(
+        table,
+        "Signature Confirmation Latency (μs)",
+        stats.signature_confirmation_latency
+    );
+    add_stats_row!(
+        table,
+        "Transactions Per Second (TPS)",
+        stats.transactions_per_second
+    );
+
+    table.printstd();
 }
